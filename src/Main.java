8ddd44e5b987c5b1c2f7f5afdcab963fb1c43b9f
exteriorduke319 <yarnevictanner@gmail.com>
2023-11-08 09:36:41 -0500

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        String initWord;
+        String palReverse = "";
+        String reverser;
+        int palCheck;
+
+        System.out.println("Please enter a word: ");
+        initWord = scan.nextLine();
+        for(int i = initWord.length()-1; i >= 0; i--) {
+            reverser = initWord.substring(i, i+1);
+            palReverse += reverser;
+        }
+
+        palCheck = initWord.compareTo(palReverse);
+        if (palCheck == 0) {
+            System.out.println(initWord + " is a palindrome.");
+        } else {
+            System.out.println(initWord + " is not a palindrome.");
+        }
     }
 }
